c1c7367b819fa7dc6721c229de95f9215181c1e2
JoaoGiatti <joaogiatti2006@gmail.com>
2024-05-14 08:55:57 -0300

    Funções fase 3
    Alterar, excluir, classificar, zerar e criar
    
    tudo no jeitinho XD

--- python/PI-fase2.py
+++ python/PI-fase2.py
@@ -7,8 +7,8 @@ init()
 # Tirar comentários do CREATE TABLE caso ainda não possua a tabela criada, depois, o CREATE é inútil
 
 Connection = oracledb.connect(
-    user = "",
-    password = "",
+    user = "BD150224214",
+    password = "Fbxfk9",
     dsn = "172.16.12.14/xe"
 )
 cursor = Connection.cursor()
@@ -78,7 +78,7 @@ def cadastro():
     # se houver, vai ser o valor do ultimo codigo_produto + 1
     else:
         cursor.execute("""
-        SELECT MAX(codigo_produto)
+        SELECT MAX(Codigo_produto)
         FROM produtos
         """)
         ultimo_codigo = cursor.fetchone()[0] # obtendo o valor da consulta em integer para realizar a soma
@@ -97,20 +97,70 @@ def tabela():
 ================================================
 CÓD. | NOME | DESCRIÇÃO | CP | CF | CV | IV | ML
 ''')
-    for row in cursor.execute("SELECT * FROM produtos"):
+    for row in cursor.execute("SELECT * FROM produtos ORDER BY codigo_produto ASC"):
         print(row)
     print(Fore.RESET)
 
 def alterar():
-    print(Fore.BLUE + "\nAinda trabalhando nisso! :)" + Fore.RESET)
+    print(Fore.GREEN + '''
+================================================
+     Aqui esão os produtos já cadastrados:
+================================================
+CÓD. | NOME | DESCRIÇÃO | CP | CF | CV | IV | ML
+''')
+    for row in cursor.execute("SELECT * FROM produtos ORDER BY codigo_produto ASC"):
+        print(row)
+    print(Fore.RESET)
+
+    codAlterar = int(input("Digite o código do produto que deseja alterar: "))
+
+    print(Fore.RED + '''
+================================================
+              Alterar Produto
+================================================
+    ''')
+    #   cod = int(input("Digite o código do produto: "))
+    nome = input("Digite o novo nome do produto: ")
+    desc = input("Digite a nova descrição do produto: ")
+    cp = float(input("Digite o novo custo do produto: "))
+    cf = float(input("Digite o novo custo fixo/administrativo: "))
+    cv = float(input("Digite o novo valor da comissão de vendas: "))
+    iv = float(input("Digite o novo valor do imposto sobre o produto: "))
+    ml = float(input("Digite o novo valor da rentabilidade: "))
+    print(Fore.RESET)
+
+    cursor.execute(f"""
+                    UPDATE produtos
+                    SET codigo_produto = {codAlterar}, nome_produto = '{nome}', Descrição_produto = '{desc}', custo_produto = {cp}, custo_fixo = {cf}, comissão_vendas = {cv}, impostos = {iv}, rentabilidade = {ml}
+                    WHERE codigo_produto = {codAlterar}
+                    """)
+    cursor.execute("COMMIT")
+
+    print(Fore.LIGHTMAGENTA_EX + "Produto alterado com sucesso!" + Fore.RESET)
 
 def excluir():
-    print(Fore.BLUE + "\nAinda trabalhando nisso! :)"+ Fore.RESET)
+    print(Fore.GREEN + '''
+================================================
+     Aqui esão os produtos já cadastrados:
+================================================
+CÓD. | NOME | DESCRIÇÃO | CP | CF | CV | IV | ML
+''')
+    for row in cursor.execute("SELECT * FROM produtos ORDER BY codigo_produto ASC"):
+        print(row)
+    print(Fore.RESET)
+
+    codExcluir = int(input("Digite o código do produto que deseja excluir: "))
+
+    cursor.execute(f"""
+                    DELETE FROM produtos WHERE codigo_produto = {codExcluir}
+                    """)
+    cursor.execute("COMMIT")
+    print(Fore.LIGHTMAGENTA_EX + "\nProduto excluido com sucesso!" + Fore.RESET)
 
 def criarTabela():
     cursor.execute("""
                 CREATE TABLE Produtos (
-                Código_produto INTEGER PRIMARY KEY NOT NULL,
+                Codigo_produto INTEGER PRIMARY KEY NOT NULL,
                 Nome_produto VARCHAR2 (20) not null,
                 Descrição_produto VARCHAR2 (50) not null,
                 Custo_produto INTEGER not null,
@@ -124,7 +174,7 @@ def zerarTabela():
     cursor.execute("DROP TABLE produtos")
     cursor.execute("""
                 CREATE TABLE Produtos (
-                Código_produto INTEGER PRIMARY KEY NOT NULL,
+                Codigo_produto INTEGER PRIMARY KEY NOT NULL,
                 Nome_produto VARCHAR2 (20) not null,
                 Descrição_produto VARCHAR2 (50) not null,
                 Custo_produto INTEGER not null,
@@ -136,7 +186,51 @@ def zerarTabela():
 
     print(Fore.LIGHTMAGENTA_EX + "\nTabela zerada com sucesso!" + Fore.RESET)
 
-while comando != 5:
+def ClassificarLucro():
+    # print(Fore.LIGHTMAGENTA_EX + "\nAinda trabalhando nisso!" + Fore.RESET)
+
+    cursor.execute('SELECT * FROM produtos ORDER BY codigo_produto ASC')
+    for row in cursor:
+        cod = row[0]
+        nome = row[1]
+        desc = row[2]
+        cp = row[3]
+        cf = row[4]
+        cv = row[5]
+        iv = row[6]
+        ml = row[7]
+
+        PV=(cp/(1-((cf+cv+iv+ml)/100)))
+
+        print(Fore.YELLOW)
+        print(row)
+        print(Fore.RESET)
+
+        print(f'''
+    Descrição ----------------- Valor - % 
+    A.Preço de venda            {round(PV,2)}   100%
+    B.Custo de Aquisição        {round(cp,2)}   {round(((cp*100)/PV),2)}%
+    C.Receita Bruta             {round((PV-cp),2)}  {round((100-(cp*100)/PV),2)}% 
+    D.Custo fixo/Administrativo {round((PV*(cf/100)),2)}  {cf}%
+    E.Comisão de vendas         {round(((cv/100)*PV),2)}  {cv}% 
+    F.Impostos                  {round(PV*(iv/100),2)}  {iv}% 
+    G.Outros custos             {round(((PV*(cf/100))+((cv/100)*PV)+((iv/100)*PV)),2)}  {cf+cv+iv}%
+    H.Rentabilidade             {round((PV-cp)-((PV*(cf/100))+((cv/100)*PV)+((iv/100)*PV)),2)}  {round(((100-(cp*100)/PV) -(cf+cv+iv)),2)}%\n
+    ''')
+        
+        if ((100-(cp*100)/PV) -(cf+cv+iv) < 0):
+            print("------- PREJUIZO -------\n")
+        elif((100-(cp*100)/PV) -(cf+cv+iv) == 0):
+            print("------- EQUILÍBRIO -------\n")
+        elif(((100-(cp*100)/PV) -(cf+cv+iv) > 0) and ((100-(cp*100)/PV) -(cf+cv+iv) <= 10)):
+            print("------- LUCRO BAIXO -------\n")
+        elif(((100-(cp*100)/PV) -(cf+cv+iv) > 10) and ((100-(cp*100)/PV) -(cf+cv+iv) <= 20)):
+            print("------- LUCRO MÉDIO -------\n")
+        else:
+            print("------- LUCRO ALTO -------\n")
+        print(f"O lucro do(a) {nome} é de {round((100-(cp*100)/PV) -(cf+cv+iv), 2)}% (R${round((PV-cp)-((PV*(cf/100))+((cv/100)*PV)+((iv/100)*PV)),2)})\n" + Fore.RESET)
+
+while comando != 8:
     comando = int(input('''
 ================================================
                 Bem-vindo(a)!
@@ -147,10 +241,11 @@ while comando != 5:
 | [4] Excluir produto                          |
 | [5] Criar tabela produtos                    |
 | [6] Zerar tabela                             |
-| [7] Sair                                     |
+| [7] Classificar Lucro                        |
+| [8] Sair                                     |
 ================================================
 
-Digite o número o comando: '''))
+Digite o número do comando: '''))
 
     if comando == 1:
         cadastro()
@@ -164,5 +259,7 @@ Digite o número o comando: '''))
         criarTabela()
     elif comando == 6:
         zerarTabela()
+    elif comando == 7:
+        ClassificarLucro()
     else:
         break
